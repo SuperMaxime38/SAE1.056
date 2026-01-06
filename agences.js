b3ae76380711d42f3f5522617b1e775c2e8d12b0
SuperMaxime38 <106980741+SuperMaxime38@users.noreply.github.com>
2026-01-06 09:59:26 +0100

    agecnes.js

--- agences.js
+++ agences.js
@@ -18,35 +18,35 @@ agences_data = {
         ],
         "tel": "+33 4 76 45 56 67"
     },
-    
+
     "agence_3": {
-        "map_url": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3976.532401793785!2d5.7309767891287455!3d45.19048085297762!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x478af48b24ee5d87%3A0xf76d4b15a3cfc863!2sLa%20Fabrique%20-%20Orange%20Business!5e0!3m2!1sfr!2sfr!4v1764941966967!5m2!1sfr!2sfr",
-        "adresse": "69 rue des Poutres, 38000 Grenoble",
+        "map_url": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2811.7668644338737!2d5.714519676768992!3d45.1918153710709!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x478af486d4db2145%3A0x5855b4bb6787a494!2sI.U.T.2%20Institut%20Universitaire%20de%20Technologie%20-%20B%C3%A2timent%20Doyen%20Gosse!5e0!3m2!1sfr!2sfr!4v1767683386294!5m2!1sfr!2sfr",
+        "adresse": "2 Place Doyen Gosse, 38000 Grenoble",
         "horaires": [
-            ["08:30-12:30","08:30-12:30","08:30-12:30","08:30-12:30","08:30-12:30","Fermé","Fermé"],
-            ["13:30-17:30","13:30-17:30","13:30-17:30","13:30-17:30","13:30-16:30","Fermé","Fermé"]
+            ["08:00-12:00","08:00-12:00","08:00-12:00","08:00-12:00","08:00-12:00","Fermé","Fermé"],
+            ["14:00-18:00","14:00-18:00","14:00-18:00","Fermé","14:00-18:00","Fermé","Fermé"]
         ],
-        "tel": "+33 4 76 44 50 50"
+        "tel": "+33 4 76 28 45 09"
     },
     
     "agence_4": {
-        "map_url": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3976.532401793785!2d5.7309767891287455!3d45.19048085297762!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x478af48b24ee5d87%3A0xf76d4b15a3cfc863!2sLa%20Fabrique%20-%20Orange%20Business!5e0!3m2!1sfr!2sfr!4v1764941966967!5m2!1sfr!2sfr",
-        "adresse": "69 rue des Poutres, 38000 Grenoble",
+        "map_url": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d89847.27340727611!2d5.5797793182770645!3d45.27352312536464!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x478ae4460d1a2de9%3A0x98b8d21788e7e4b4!2sLyc%C3%A9e%20Polyvalent%20Ferdinand%20Buisson%20(%20dit%20%22La%20NAT%22)!5e0!3m2!1sfr!2sfr!4v1767683585343!5m2!1sfr!2sfr",
+        "adresse": "21 Boulevard Edgar Kofler, 38500 Voiron",
         "horaires": [
-            ["08:30-12:30","08:30-12:30","08:30-12:30","08:30-12:30","08:30-12:30","Fermé","Fermé"],
-            ["13:30-17:30","13:30-17:30","13:30-17:30","13:30-17:30","13:30-16:30","Fermé","Fermé"]
+            ["08:00-12:00","08:00-12:00","08:00-12:00","08:00-12:00","08:00-12:00","Fermé","Fermé"],
+            ["14:00-18:00","14:00-18:00","Fermé","14:00-18:00","14:00-18:00","Fermé","Fermé"]
         ],
-        "tel": "+33 4 76 44 50 50"
+        "tel": "+33 4 76 05 83 90"
     },
     
     "agence_5": {
-        "map_url": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3976.532401793785!2d5.7309767891287455!3d45.19048085297762!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x478af48b24ee5d87%3A0xf76d4b15a3cfc863!2sLa%20Fabrique%20-%20Orange%20Business!5e0!3m2!1sfr!2sfr!4v1764941966967!5m2!1sfr!2sfr",
-        "adresse": "69 rue des Poutres, 38000 Grenoble",
+        "map_url": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d547742.8801428896!2d4.379829483720352!3d45.936399538595694!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f4ec854224d8ab%3A0x2166d105dc815ee!2sOrange!5e0!3m2!1sfr!2sfr!4v1767684110695!5m2!1sfr!2sfr",
+        "adresse": "CENTRE COMMERCIAL, Grand Ouest, Le Perollier, 69130 Écully",
         "horaires": [
-            ["08:30-12:30","08:30-12:30","08:30-12:30","08:30-12:30","08:30-12:30","Fermé","Fermé"],
-            ["13:30-17:30","13:30-17:30","13:30-17:30","13:30-17:30","13:30-16:30","Fermé","Fermé"]
+            ["09:30","09:30","09:30","09:30","09:30","09:30","Fermé"],
+            ["20:00","20:00","20:00","20:00","20:00","20:00","Fermé"]
         ],
-        "tel": "+33 4 76 44 50 50"
+        "tel": "+33 4 76 45 18 32"
     }
 
 };
